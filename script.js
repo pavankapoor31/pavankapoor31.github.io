9eb784e7fce3174111247bcf18c112e9eae91d5c
Pavan Kapoor <pavankapoor31@gmail.com>
2022-06-12 16:06:20 +0530

    Portfolio

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+    })
+});
+
+// toggling menu/navbar 
+$('.menu-btn').click(function() {
+
+    $('.navbar .menu').toggleClass("active");
+
+});
+$('.menu-btn').onClick(function() {
+
+    $('.navbar .menu').toggleClass("active");
+
+});
+
+function toggleClass() {
+
+    $('.navbar .menu').toggleClass("active");
+
+};
